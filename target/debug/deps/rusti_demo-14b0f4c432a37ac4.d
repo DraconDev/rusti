ca51e52b592ac0e7c2d954dd1c6572ca21178ed1
DraconDev <dracsharp@gmail.com>
2025-11-19 19:47:07 +0000

    fix(deps): Add missing dependencies and fix compilation errors

---
+++ target/debug/deps/rusti_demo-14b0f4c432a37ac4.d
@@ -0,0 +1,6 @@
+/home/dracon/_Dev/gemini_project/rusti/target/debug/deps/rusti_demo-14b0f4c432a37ac4.d: demo/src/main.rs demo/src/examples/mod.rs
+
+/home/dracon/_Dev/gemini_project/rusti/target/debug/deps/rusti_demo-14b0f4c432a37ac4: demo/src/main.rs demo/src/examples/mod.rs
+
+demo/src/main.rs:
+demo/src/examples/mod.rs:
