012bb546ed014fd264eca765177d929b2ec12acc
DraconDev <dracsharp@gmail.com>
2025-11-20 05:08:22 +0000

    feat(macros): Update rustc_info and build artifacts

--- target/debug/deps/rusti_macros-6faf2e9ed51efcc9.d
+++ target/debug/deps/rusti_macros-6faf2e9ed51efcc9.d
@@ -1,8 +1,9 @@
-/home/dracon/_Dev/gemini_project/rusti/target/debug/deps/rusti_macros-6faf2e9ed51efcc9.d: macros/src/lib.rs macros/src/parser.rs macros/src/tests.rs macros/src/attr_tests.rs
+/home/dracon/_Dev/gemini_project/rusti/target/debug/deps/rusti_macros-6faf2e9ed51efcc9.d: macros/src/lib.rs macros/src/parser.rs macros/src/parser_test.rs macros/src/tests.rs macros/src/attr_tests.rs
 
-/home/dracon/_Dev/gemini_project/rusti/target/debug/deps/rusti_macros-6faf2e9ed51efcc9: macros/src/lib.rs macros/src/parser.rs macros/src/tests.rs macros/src/attr_tests.rs
+/home/dracon/_Dev/gemini_project/rusti/target/debug/deps/rusti_macros-6faf2e9ed51efcc9: macros/src/lib.rs macros/src/parser.rs macros/src/parser_test.rs macros/src/tests.rs macros/src/attr_tests.rs
 
 macros/src/lib.rs:
 macros/src/parser.rs:
+macros/src/parser_test.rs:
 macros/src/tests.rs:
 macros/src/attr_tests.rs:
