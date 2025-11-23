31e9699211d256cc2c6039072d974efcb0915764
DraconDev <dracsharp@gmail.com>
2025-11-23 13:25:45 +0000

    feat: Add a utility function for basic HTML and a development todo list.

---
+++ demo/src/basic_html_no_parser.rs
@@ -0,0 +1,12 @@
+pub fn get_basic_html() -> &'static str {
+    r#"<!DOCTYPE html>
+<html>
+    <head>
+        <title>Basic HTML Example</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+        <p>This is a simple HTML page.</p>
+    </body>
+</html>"#
+}
---
+++ todo.md
@@ -0,0 +1,3 @@
+datastar doesnt seem to work, the advanced examples also seems to cause trouble iwth the single ' in the html
+
+we need to update the docs to include the "" or {} exceptions for rate cases like 2em
