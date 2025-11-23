b814d5160622ac356e5fbb8c65334fb710fb3af0
DraconDev <dracsharp@gmail.com>
2025-11-23 15:05:57 +0000

    fix(demo): Correct typo in basic page HTML content

--- demo/src/basic_page.rs
+++ demo/src/basic_page.rs
@@ -38,7 +38,7 @@ pub fn basic_page() -> impl rusti::Component {
                 <script>
                     console.log("Hello from Rusti!");
                 </script>
-                'aa'
+                '_
             </body>
         </html>
     }
