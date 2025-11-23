fb4f3f7d74f78f490367c63df9b6ebd4213f82d2
DraconDev <dracsharp@gmail.com>
2025-11-23 12:23:11 +0000

    refactor(demo): Inline CSS in basic\_page component

--- demo/src/basic_page.rs
+++ demo/src/basic_page.rs
@@ -3,17 +3,6 @@ use axum::response::{Html, IntoResponse};
 use rusti::rusti;
 
 pub fn basic_page() -> impl rusti::Component {
-    let styles = r#"
-        body {
-            font-family: sans-serif;
-            margin: 2em;
-            background-color: #f4f4f4;
-            color: #333;
-        }
-        h1 {
-            color: #0056b3;
-        }
-    "#;
     rusti! {
         <html lang="en">
             <head>
@@ -29,7 +18,17 @@ pub fn basic_page() -> impl rusti::Component {
                 <!-- <meta property="og:title" content="Basic Page" /> -->
 
                 <!-- You can also include inline styles if necessary -->
-                <style>{styles}</style>
+                <style>
+                    body {
+                        font-family: sans-serif;
+                        margin: 2em;
+                        background-color: #f4f4f4;
+                        color: #333;
+                    }
+                    h1 {
+                        color: #0056b3;
+                    }
+                </style>
             </head>
             <body>
                 <!-- Main content section -->
