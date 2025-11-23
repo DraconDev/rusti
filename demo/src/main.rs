3e6ee72bb046c2d4c2983e22dfcb387a4f8322a3
DraconDev <dracsharp@gmail.com>
2025-11-23 16:11:43 +0000

    refactor(demo): Remove unused `datastar_extended` demo The file `demo/src/datastar_extended.rs` and its corresponding route in `demo/src/main.rs` have been removed as the component is no longer needed or in use.

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -9,7 +9,6 @@ mod advanced;
 mod base_layout_demo;
 mod basic_page;
 mod datastar;
-mod datastar_extended;
 mod extreme;
 mod quote_demo;
 mod styles_demo;
@@ -18,7 +17,6 @@ mod tailwind_demo;
 use about::about_page_wrapper;
 use base_layout_demo::base_layout_demo_handler;
 use datastar::datastar_demo_handler;
-use datastar_extended::datastar_extended_handler;
 use rusti::rusti;
 use serde::Deserialize;
 use styles_demo::styles_demo_handler;
@@ -689,7 +687,6 @@ async fn main() {
         .route("/base-layout", get(base_layout_demo_handler))
         .route("/styles", get(styles_demo_handler))
         .route("/datastar", get(datastar_demo_handler))
-        .route("/datastar-extended", get(datastar_extended_handler))
         .route("/styles2", get(styles_demo2_handler))
         .route("/forms", get(forms_demo))
         .route("/tailwind", get(tailwind_demo_handler))
