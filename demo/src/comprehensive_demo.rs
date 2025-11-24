47eed783e7da9c095bb27e4fe4ea63bc0529ed34
DraconDev <dracsharp@gmail.com>
2025-11-24 12:43:46 +0000

    fix(demo): Correct typo in CSS background-color property

--- demo/src/comprehensive_demo.rs
+++ demo/src/comprehensive_demo.rs
@@ -14,7 +14,7 @@ pub fn comprehensive_demo() -> impl rusti::Component {
                     margin: 0;
                     padding: 0;
                     box-sizing: border-box;
-                    background-color: yellow;
+                    baackground-color: yellow;
                 }
 
                 body {
