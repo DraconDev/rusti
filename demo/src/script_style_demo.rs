4aff155b04dab7dbf396dceadaf5e7cdf4086c22
DraconDev <dracsharp@gmail.com>
2025-11-24 18:28:23 +0000

    feat(demo): Update script style demo to use formatted string for variable injection

--- demo/src/script_style_demo.rs
+++ demo/src/script_style_demo.rs
@@ -78,7 +78,9 @@ pub fn script_style_demo() -> impl rusti::Component {
                 console.log("Config loaded:", config);
 
                 // Injecting Rust variables using @{ ... }
-                const userName = "@{ user_name }";
+                // const str = @{ format!("\"{}\"", my_string) };
+
+                const userName = @{ format!("\"{}\"", user_name) };
                 const items = [
                     // Using @for loop to generate JS array elements
                     @for item in &items {
