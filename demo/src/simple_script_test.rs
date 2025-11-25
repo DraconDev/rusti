b8f6cfe53110f1e700a76a7d52749d71516e7500
DraconDev <dracsharp@gmail.com>
2025-11-25 00:35:38 +0000

    feat(demo): Simplify script expression evaluation in test Removes unnecessary `format!` calls when embedding Rust variables directly into JavaScript constants within the simple script test. Also enables a previously commented-out working loop.

--- demo/src/simple_script_test.rs
+++ demo/src/simple_script_test.rs
@@ -19,10 +19,10 @@ pub fn simple_script_test() -> impl rusti::Component {
             <script>
                 @let my_string = "Hello".to_string();
                 const num = @{ my_number };
-                const str = @{ format!("\"{}\"", my_string) };
+                const str = @{ my_string };
                 // const str = @{ my_string }; doesn't work
                 // const str = @{ my_string.to_string() }; doesn't work
-                const arr = @{ format!("{:?}", my_array) }; // this works too
+                const arr = @{ my_array };
                 // const arr = @{ my_array }; doesn't work, even errors in compiler
                 // const arr = new Array (@{ my_array }); doesn't work
 
@@ -38,9 +38,9 @@ pub fn simple_script_test() -> impl rusti::Component {
 
                 @let my_string_array = vec!["Apple", "Banana", "Cherry"];
 
-                // @for item in &my_string_array {
-                //     document.getElementById("output").innerHTML += "<br>" + @{ item };
-                // } // this doesn't work, simple-script-test:1 Uncaught ReferenceError: Apple is not defined
+                @for item in &my_string_array {
+                    document.getElementById("output").innerHTML += "<br>" + @{ item };
+                } // this works now!
 
                 @for item in &my_string_array {
                     document.getElementById("output").innerHTML += "<br>" + @{ format!("\"{}\"", item) };
