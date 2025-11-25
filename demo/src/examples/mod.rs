9e5d796e006bd3eaa9b36e6902a51790b3ac854f
DraconDev <dracsharp@gmail.com>
2025-11-25 21:06:40 +0000

    refactor(demo): remove unnecessary module re-exports in examples mod

--- demo/src/examples/mod.rs
+++ demo/src/examples/mod.rs
@@ -6,9 +6,3 @@ pub mod homepage;
 pub mod htmx_todo;
 pub mod layouts;
 pub mod tailwind;
-
-pub use components::*;
-pub use hello::*;
-pub use homepage::*;
-pub use htmx_todo::*;
-pub use tailwind::*;
