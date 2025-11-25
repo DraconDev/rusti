59351fffb1ef04aea52d9ba94ba9b72917c53a98
DraconDev <dracsharp@gmail.com>
2025-11-25 03:32:48 +0000

    refactor(todo): Simplify `add_todo` handler by removing boilerplate and logic stub

--- demo/src/handlers/todo.rs
+++ demo/src/handlers/todo.rs
@@ -1,52 +1,6 @@
 use axum::{body::Body, response::IntoResponse};
 
-use axum::{
-    extract::{State, Json},
-    http::StatusCode,
-    response::IntoResponse,
-};
-use std::{
-    sync::{Arc, Mutex},
-    collections::HashMap,
-};
-use serde::{Deserialize, Serialize};
-
-// Define your Todo struct
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Todo {
-    pub id: u32,
-    pub title: String,
-    pub completed: bool,
-}
-
-// Struct for creating a new todo (without an ID)
-#[derive(Debug, Deserialize)]
-pub struct CreateTodo {
-    pub title: String,
-}
-
-// Type alias for our shared state
-type SharedState = Arc<Mutex<HashMap<u32, Todo>>>;
-
-pub async fn add_todo(
-    State(todos): State<SharedState>,
-    Json(create_todo): Json<CreateTodo>,
-) -> impl IntoResponse {
-    let mut todos = todos.lock().unwrap();
-
-    // Generate a simple ID. In a real app, you might use a UUID or a more robust counter.
-    let id = todos.keys().max().map_or(1, |max_id| max_id + 1);
-
-    let new_todo = Todo {
-        id,
-        title: create_todo.title,
-        completed: false,
-    };
-
-    todos.insert(id, new_todo.clone());
-
-    (StatusCode::CREATED, Json(new_todo))
-}
+pub async fn add_todo() -> impl IntoResponse {}
 
 pub async fn clear_completed() -> impl IntoResponse {}
 
