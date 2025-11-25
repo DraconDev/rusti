c845599f4acdaa673303356eb623e49619261fea
DraconDev <dracsharp@gmail.com>
2025-11-25 15:09:06 +0000

    fix(test): Uncomment one error test case in demo

--- demo/src/error_test.rs
+++ demo/src/error_test.rs
@@ -4,13 +4,13 @@ fn main() {
     // Uncomment one by one to test error messages
 
     // 1. Unclosed element
-    rusti! { <div> };
+    // rusti! { <div> };
 
     // 2. Mismatched closing tag
     // rusti! { <div></span> };
 
     // 3. Invalid attribute
-    // rusti! { <div class=></div> };
+    rusti! { <div class=></div> };
 
     // 4. Missing expression brace
     // rusti! { @if true <div></div> };
