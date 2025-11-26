9a729a81c31968330df67b83ba5e383440c254cc
DraconDev <dracsharp@gmail.com>
2025-11-26 16:02:58 +0000

    feat(deps): Upgrade azumi to 1.3.90 and refactor demo This commit updates the `azumi` dependency version and makes associated changes in the demo: removing an unused CSS file and updating component markup to include styles reference.

--- demo/src/examples/lessona.rs
+++ demo/src/examples/lessona.rs
@@ -1,10 +1,10 @@
-use axum::response::{Html, IntoResponse};
 use azumi::html;
 
 /// Lesson 0: Getting Started - Your first Azumi component
 pub fn lessona() -> impl azumi::Component {
     html! {
-        <div class="cente">
+        <div class="center-te">
+            <styles src="/static/lessona.css" />
             <h1>"Hello from Azumi!"</h1>
             <p>"This is your first lesson"</p>
             <p>"Azumi makes HTML type-safe"</p>
