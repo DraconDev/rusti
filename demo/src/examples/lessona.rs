58d27d268381778875a43cfa97a4ccac77e84dd7
DraconDev <dracsharp@gmail.com>
2025-11-26 16:02:27 +0000

    feat(azumi): Upgrade version and update demo component to use `html!` macro

--- demo/src/examples/lessona.rs
+++ demo/src/examples/lessona.rs
@@ -3,8 +3,11 @@ use azumi::html;
 
 /// Lesson 0: Getting Started - Your first Azumi component
 pub fn lessona() -> impl azumi::Component {
-    let simple_message = r#"<div>
-    <h1>"Hello from Azumi!"</h1>
-    <p>"This is your first lesson"</p>
-    <p>"Azumi makes HTML type-safe"</p>
-</div>"#;
+    html! {
+        <div class="cente">
+            <h1>"Hello from Azumi!"</h1>
+            <p>"This is your first lesson"</p>
+            <p>"Azumi makes HTML type-safe"</p>
+        </div>
+    }
+}
