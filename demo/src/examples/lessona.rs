2451a25b5a3e4e0b94f3ca4d30d06529744f1803
DraconDev <dracsharp@gmail.com>
2025-11-26 16:07:14 +0000

    feat(deps): Update azumi to v1.3.97 and adjust demo styling

--- demo/src/examples/lessona.rs
+++ demo/src/examples/lessona.rs
@@ -4,7 +4,7 @@ use azumi::html;
 /// Lesson 0: Getting Started - Your first Azumi component
 pub fn lessona() -> impl azumi::Component {
     html! {
-        <div class="bab">
+        <div class="baasdasdasd  b">
             <styles src="/static/lessona.css" />
             <h1>"Hello from Azumi!"</h1>
             <p>"This is your first lesson"</p>
