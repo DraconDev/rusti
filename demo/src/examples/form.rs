b46bda110c4a88ed6fcad0b213f9bbea2bcf1505
DraconDev <dracsharp@gmail.com>
2025-11-28 11:35:03 +0000

    fix(deps): Update azumi to 1.7.51 and correct  emiil  typo or simply: fix: Update azumi and correct form input typo

--- demo/src/examples/form.rs
+++ demo/src/examples/form.rs
@@ -4,7 +4,7 @@ pub fn form() -> impl azumi::Component {
     html! {
         <form>
             <input type="text" name="name" />
-            <input type="email" name="emiil" />
+            <input type="email" name="email" />
             <button type="submit">"Submit"</button>
         </form>
     }
