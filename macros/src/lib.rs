9e4dc1e294fc34d54396d15002cf4b98d917fb0e
DraconDev <dracsharp@gmail.com>
2025-11-29 14:29:21 +0000

    feat(macros): Use `quote_spanned` in `lib.rs`

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -12,7 +12,7 @@ mod test_spacing;
 mod token_parser;
 
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, quote_spanned};
 use syn::parse::{Parse, ParseStream};
 use syn::parse_macro_input;
 use syn::spanned::Spanned;
