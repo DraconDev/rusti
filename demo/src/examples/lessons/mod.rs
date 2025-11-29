68fe453f4b88275aafcbc7d2701dbdf6a63a0556
DraconDev <dracsharp@gmail.com>
2025-11-29 14:03:26 +0000

    refactor(demo): Remove redundant lesson module re-exports Or alternatively, if this change simplifies things significantly: chore(demo): Clean up unused lesson module declarations

--- demo/src/examples/lessons/mod.rs
+++ demo/src/examples/lessons/mod.rs
@@ -1,12 +1,3 @@
 pub mod pages;
 
 // Register new lessons
-pub mod lesson31 {
-    pub use super::pages::lesson31::*;
-}
-pub mod lesson32 {
-    pub use super::pages::lesson32::*;
-}
-pub mod lesson33 {
-    pub use super::pages::lesson33::*;
-}
