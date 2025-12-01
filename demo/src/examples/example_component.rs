b8b810f885f9f05d8d391848cfec96570e5ce80d
DraconDev <dracsharp@gmail.com>
2025-12-01 09:38:58 +0000

    fix(deps): Update azumi dependency version in Cargo.lock

---
+++ demo/src/examples/example_component.rs
@@ -0,0 +1,33 @@
+//! Lesson 1: hello_world.rs
+//!
+//! Simple template showing basic structure
+use azumi::html;
+
+/// Simple hello world with fancy styling
+#[azumi::component]
+pub fn hello_world() -> impl azumi::Component {
+    html! {
+        <style src="/static/pages/lesson1.css" />
+        <div class="hello-container">
+            <h1 class="hello-title">"Hello Azumi!"</h1>
+        </div>
+    }
+}
+
+/// Basic template with styling
+#[azumi::component]
+pub fn basic_template() -> impl azumi::Component {
+    html! {
+        <style src="/static/pages/lesson1.css" />
+        <div class="basic-template" >
+            <h1 class="basic-h1">"Hello, World!"</h1>
+            <h2 class="basic-h2">"Welcome to Azumi"</h2>
+            <p class="basic-p">"This is a simple styled template"</p>
+        </div>
+    }
+}
+
+// Handler for Axum
+pub async fn lesson1_handler() -> impl axum::response::IntoResponse {
+    axum::response::Html(azumi::render_to_string(&hello_world()))
+}
