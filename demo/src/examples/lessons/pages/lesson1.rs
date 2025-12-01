73ca1532bab9830f82f2b9062f338b29a1339288
DraconDev <dracsharp@gmail.com>
2025-12-01 20:44:22 +0000

    fix(demo): Correct invalid CSS margin value in lesson example

--- demo/src/examples/lessons/pages/lesson1.rs
+++ demo/src/examples/lessons/pages/lesson1.rs
@@ -10,7 +10,7 @@ pub fn hello_world() -> impl azumi::Component {
         <style>
             .hello_container {
                 text-align: "center";
-                margin-top: "cake";
+                margin-top: "50px";
                 background-color: "#f00";
             }
             .hello_title {
