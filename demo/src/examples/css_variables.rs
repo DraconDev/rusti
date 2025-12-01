2b392557bb2d16ed761f09798abe078191d82c05
DraconDev <dracsharp@gmail.com>
2025-12-01 15:55:52 +0000

    feat(demo): Remove CSS variable demo and update lockfile

--- demo/src/examples/css_variables.rs
+++ demo/src/examples/css_variables.rs
@@ -2,33 +2,8 @@ use azumi::html;
 
 #[azumi::component]
 pub fn css_variables_demo() -> impl azumi::Component {
-    let percentage = "50%";
-    let color = "yellow";
-
     html! {
-        <style>
-            .progress_bar {
-                background-color: "#e0e0e0";
-                border-radius: "4px";
-                height: "20px";
-                width: "100%";
-            }
-            .progress_value {
-                background-color: "#76c7c0";
-                border-radius: "4px";
-                height: "100%";
-                width: "var(--width)";
-                transition: "width 0.5s ease-in-out";
-            }
-        </style>
-
-        <div class={progress_bar} --width={percentage}>
-            <div class={progress_value}></div>
-        </div>
-
-        <div --static-var="100px">
-            "Static Var"
-        </div>
+        <div>"Empty"</div>
     }
 }
 
