0cf8d35ad576e99fb244babe6a7f8a14fd4390bf
DraconDev <dracsharp@gmail.com>
2025-12-02 06:41:20 +0000

    refactor(demo): Remove inline styles test example

--- demo/src/examples/mod.rs
+++ demo/src/examples/mod.rs
@@ -10,4 +10,3 @@ pub mod example_component;
 pub mod form;
 pub mod test_css_validation; // CSS validation test case
 pub mod test_global_styles; // Global style tag test
-pub mod test_inline_styles; // Inline styles test
