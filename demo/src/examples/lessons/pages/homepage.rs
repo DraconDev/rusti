0503271ba577a9e655a91059b780c7c1cf6f8514
DraconDev <dracsharp@gmail.com>
2025-12-02 17:07:55 +0000

    feat(demo): Use dark layout on homepage example

--- demo/src/examples/lessons/pages/homepage.rs
+++ demo/src/examples/lessons/pages/homepage.rs
@@ -2,7 +2,7 @@ use azumi::html;
 
 #[azumi::component]
 pub fn homepage() -> impl azumi::Component {
-    modern_layout(children = lessons())
+    dark_modern_layout(children = lessons())
 }
 
 #[azumi::component]
