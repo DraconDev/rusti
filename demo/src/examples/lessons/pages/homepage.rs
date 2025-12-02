8106c69297860e3bca58eb13eb4123b96f5c87c3
DraconDev <dracsharp@gmail.com>
2025-12-02 17:12:15 +0000

    refactor(demo): Use `layout` instead of `dark_modern_layout`

--- demo/src/examples/lessons/pages/homepage.rs
+++ demo/src/examples/lessons/pages/homepage.rs
@@ -1,5 +1,5 @@
 use azumi::html;
-use crate::examples::lessons::components::dark_modern_layout;
+use crate::examples::lessons::components::layout;
 
 #[azumi::component]
 pub fn homepage() -> impl azumi::Component {
