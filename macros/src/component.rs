948db12384d26dd0fcc225d7296d53b18f61f880
DraconDev <dracsharp@gmail.com>
2025-12-04 17:22:41 +0000

    feat(macros): Wrap component body in `azumi::from_fn`

--- macros/src/component.rs
+++ macros/src/component.rs
@@ -118,15 +118,21 @@ pub fn expand_component(item: proc_macro::TokenStream) -> proc_macro::TokenStrea
 
         let body = if let Some(state_ident) = live_state_ident {
             quote! {
-                // Auto-generated az-scope wrapper
-                let scope_json = <_ as azumi::LiveState>::to_scope(#state_ident);
-                write!(f, "<div az-scope='{}' style='display: contents'>", scope_json)?;
-                #fn_block
-                write!(f, "</div>")?;
-                Ok(())
+                azumi::from_fn(move |f| {
+                    // Auto-generated az-scope wrapper
+                    let scope_json = <_ as azumi::LiveState>::to_scope(#state_ident);
+                    write!(f, "<div az-scope='{}' style='display: contents'>", scope_json)?;
+                    #fn_block
+                    write!(f, "</div>")?;
+                    Ok(())
+                })
             }
         } else {
-            quote! { #fn_block }
+            quote! {
+                azumi::from_fn(move |f| {
+                    #fn_block
+                })
+            }
         };
 
         quote! {
@@ -138,15 +144,21 @@ pub fn expand_component(item: proc_macro::TokenStream) -> proc_macro::TokenStrea
     } else {
         let body = if let Some(state_ident) = live_state_ident {
             quote! {
-                // Auto-generated az-scope wrapper
-                let scope_json = <_ as azumi::LiveState>::to_scope(#state_ident);
-                write!(f, "<div az-scope='{}' style='display: contents'>", scope_json)?;
-                #fn_block
-                write!(f, "</div>")?;
-                Ok(())
+                azumi::from_fn(move |f| {
+                    // Auto-generated az-scope wrapper
+                    let scope_json = <_ as azumi::LiveState>::to_scope(#state_ident);
+                    write!(f, "<div az-scope='{}' style='display: contents'>", scope_json)?;
+                    #fn_block
+                    write!(f, "</div>")?;
+                    Ok(())
+                })
             }
         } else {
-            quote! { #fn_block }
+            quote! {
+                azumi::from_fn(move |f| {
+                    #fn_block
+                })
+            }
         };
 
         quote! {
