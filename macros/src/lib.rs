8977fba0d659a9cfe6f6d643741987c061263d3e
DraconDev <dracsharp@gmail.com>
2025-12-05 06:36:22 +0000

    feat(azumi): Add debug logging to style DSL macro expansion

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1023,6 +1023,7 @@ fn generate_body_with_context(
                                 }
 
                                 attr_code.extend(quote! {
+                                    eprintln!("DEBUG STYLE DSL: {}", format!(#format_string, #(#args),*));
                                     write!(f, " style=\"{}\"", format_args!(#format_string, #(#args),*))?;
                                 });
                             }
